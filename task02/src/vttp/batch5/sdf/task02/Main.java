82573dd16a8ff17d99b275927a0c52c7a06ff2d9
s-phuar <coolbeansphuar123@gmail.com>
2024-10-30 15:35:01 +0800

    updated code for cleanliness

--- task02/src/vttp/batch5/sdf/task02/Main.java
+++ task02/src/vttp/batch5/sdf/task02/Main.java
@@ -15,11 +15,9 @@ import java.util.ArrayList;
 public class Main extends TTTengine {
 
     char[][] board;
-    char currentPlayer;
 
     public Main(){
         board = new char[3][3];
-        currentPlayer = 'X';   	//X (me) starts
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
                 board[i][j] = '.';
